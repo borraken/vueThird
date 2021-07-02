8243ecfe994278f6f34872a3ef97a9ab8a8c970a
borraken <61871816+borraken@users.noreply.github.com>
2021-07-02 16:35:44 +0800

    1

---
+++ precache-manifest.3d7404551127bcb568a4d78cdfa4eede.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914898bbb2cf2daefeae",
+    "url": "/vueThird/css/app.4cfc6ce0.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vueThird/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8b90d6caf99c6ba651e4628b3a1530a6",
+    "url": "/vueThird/index.html"
+  },
+  {
+    "revision": "23f20846e87771754caf",
+    "url": "/vueThird/js/about.9bb65978.js"
+  },
+  {
+    "revision": "914898bbb2cf2daefeae",
+    "url": "/vueThird/js/app.3720a8b6.js"
+  },
+  {
+    "revision": "f4910a0d991fb29d7fe5",
+    "url": "/vueThird/js/chunk-vendors.05e0863f.js"
+  },
+  {
+    "revision": "6df42186fc4c521ee4a543a0579aa068",
+    "url": "/vueThird/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vueThird/robots.txt"
+  }
+]);
